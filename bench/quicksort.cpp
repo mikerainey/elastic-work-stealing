3f43fa8de4a32b4088db26c36792d70c638d142f
mikerainey <me@mike-rainey.site>
2019-12-18 15:43:41 -0500

    have quicksort benchmark use pretty good parallel quicksort algorithm

--- bench/quicksort.cpp
+++ bench/quicksort.cpp
@@ -7,14 +7,14 @@ using namespace pbbs;
 
 int main(int argc, char** argv) {
   using T = double;
+  long cutsize = deepsea::cmdline::parse_or_default_int("cutsize", -1);
   auto infile = deepsea::cmdline::parse_or_default_string("infile", "sequence.txt");
   auto D = benchIO::readSequenceFromFile(infile.c_str());
   sequence<T> in((T*)D.A, D.n);
-  sequence<T> out;
 
   pbbsBench::launch([&] {
-    out = p_quicksort(in, less<T>());
+    quicksort(in.begin(), D.n, less<T>(), cutsize);
   });
   
-  cout << "result " << out.size() << endl;
+  cout << "result " << *in.begin() << endl;
 }
