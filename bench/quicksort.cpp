0a7b02b80434c80c6d1713906599bd21a547d782
mikerainey <me@mike-rainey.site>
2019-12-18 15:46:52 -0500

    fix

--- bench/quicksort.cpp
+++ bench/quicksort.cpp
@@ -7,7 +7,7 @@ using namespace pbbs;
 
 int main(int argc, char** argv) {
   using T = double;
-  long cutsize = deepsea::cmdline::parse_or_default_int("cutsize", -1);
+  long cutsize = deepsea::cmdline::parse_or_default_int("cutsize", 1<<10);
   auto infile = deepsea::cmdline::parse_or_default_string("infile", "sequence.txt");
   auto D = benchIO::readSequenceFromFile(infile.c_str());
   sequence<T> in((T*)D.A, D.n);
